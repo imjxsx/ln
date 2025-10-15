7e724f2651cdc44b40918d7a6db4e3c8916d1239
I'm Jxsx <jxsxdxnxxl@gmail.com>
2025-10-15 10:45:52 -0300

    Delete build directory

--- build/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type Logger from "logger";
-export type Nullable<T> = T | undefined | null;
-export interface LnOptions {
-    default: string;
-    directory: string;
-    logger?: Nullable<Logger>;
-}
